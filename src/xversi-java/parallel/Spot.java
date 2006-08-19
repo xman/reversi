ce49237ba33a5f8755d9655e4c75bb1ffc54d80d
xman <xman>
2006-08-19 20:37:49 +0000

    [UPDATE] Included GPL license to release as open source software. [UPDATE] Changed to nodeinfo.txt to include only localhost. [UPDATE] Recompile all the .java files to get new .class files.

--- src/xversi-java/parallel/Spot.java
+++ src/xversi-java/parallel/Spot.java
@@ -1,3 +1,29 @@
+/*
+# This file Spot.java is part of xversi-java-parallel, a board game called reversi or othello.
+#
+# Copyright (c) 2002-2006 Chung Shin Yee <cshinyee@gmail.com>
+#
+#       http://cshinyee.blogspot.com/index.html
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+#
+# The GNU General Public License is contained in the file COPYING.
+#
+*/
+
 /* 2D coordinate */
 public class Spot {
 
